0b8f9fd54790be20e80cfeadba45431dec4428d0
hochal <748868@gmail.com>
2018-10-07 18:59:25 +0900

    ch.2.3.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
 // 걸프 의존성을 여기 씁니다.
+const babel = require('gulp-babel');
 
 gulp.task('default', function(callback){
-    // 걸프 작업을 여기 씁니다.
-    console.log('Hi');
+    // 노드 소스
+    gulp.src('es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
+    // 브라우저 소스
+    gulp.src('public/es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('public/dist'));
     callback();
 });
