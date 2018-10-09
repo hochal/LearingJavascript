141d851f6c4b6fcf7d3753a8ddc443b948d9bfff
hochal <748868@gmail.com>
2018-10-09 18:26:16 +0900

    ch.2.4

--- public/es6/test.js
+++ public/es6/test.js
@@ -13,6 +13,7 @@ const sentences = [
         object: 'large'
     }
 ];
+
 // es6 기능: 객체 분해
 function say({subject, verb, object}) {
     // es6 기능: 템플릿 문자열
@@ -20,7 +21,8 @@ function say({subject, verb, object}) {
     // 키보드의 탭 키 바로 위에 있는 문자입니다.
     console.log(`${subject} ${verb} ${object}`);
 }
+
 // es6 기능: for..of
-for(let s of sentences) {
+for (let s of sentences) {
     say(s);
 }
