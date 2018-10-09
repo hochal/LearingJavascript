1a727ece3c2a4bab4ef7726f3e4c2805be91d9d2
hochal <748868@gmail.com>
2018-10-09 20:08:09 +0900

    ch.3.8.2

---
+++ es6/ch.3.8.2.js
@@ -0,0 +1,24 @@
+// const multiLine = 'line1\
+// line2';
+
+// const multiLine = 'line1\n\
+// line2';
+
+// const multiLine = `line1
+// line2`;
+
+// const multiLine = `line1
+//     line2
+//     line3`;
+
+// const multiLine = 'line1\n' +
+//     'line2\n' +
+//     'line3';
+
+const currentTemp = 19.5;
+
+const multiLine = 'Current temperature:\n' +
+    `\t${currentTemp}\u00b0C\n` +
+    "Don't worry...the heat is on!";
+
+console.log(multiLine);
