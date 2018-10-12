a133b0206b366adabb921188b9ddaba403cd544d
hochal <748868@gmail.com>
2018-10-13 03:40:00 +0900

    ch.7.3

---
+++ es6/ch.7.3.js
@@ -0,0 +1,32 @@
+let name = "Irena"; // global
+let age = 25;   // global
+
+function greet() {
+    console.log(`Hello, ${name}!`);
+}
+
+function getBirthYear() {
+    return new Date().getFullYear() - age;
+}
+
+let user = {
+    name: "Irena",
+    age: 25,
+};
+
+function greet() {
+    console.log(`Hello, ${user.name}!`);
+}
+
+function getBirthYear() {
+    return new Date().getFullYear() - user.age;
+}
+
+
+function greet(user) {
+    console.log(`Hello, ${user.name}!`);
+}
+
+function getBirthYear(user) {
+    return new Date().getFullYear() - user.age;
+}
