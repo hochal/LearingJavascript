bc7895a4987beb9923b43a772e433fdb8f087955
hochal <748868@gmail.com>
2018-10-12 16:26:14 +0900

    ch.6.4

---
+++ es6/ch.6.4.js
@@ -0,0 +1,11 @@
+const o={
+    name: 'Wallace', // 원시 값 프로퍼티
+    bark: function() { return 'Woof!'; }, // 함수 프포퍼티(메서드)
+};
+
+const o2={
+    name: 'Wallace', // 원시 값 프로퍼티
+    bark() { return 'Woof!'; }, // 함수 프포퍼티(메서드)
+};
+
+
